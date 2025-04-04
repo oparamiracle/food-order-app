9d2898402eaa44d95936561d102802f5bd35265f
Opara Blessing <blessingdvd600@gmail.com>
2025-04-04 03:11:21 +0100

    Add new files and modify admin management pages

---
+++ admin/manage-admin.php
@@ -0,0 +1,54 @@
+<?php include('partials/menu.php'); ?>
+
+
+<!-- Main content starts -->
+<div class="main-content">
+  <div class="wrapper">
+           <h1 >Manage Admin</h1>
+           <br /> <br />
+           <!-- button to add admin -->
+            <a href="#" class="btn-primary">Add Admin</a>
+          <br /> <br />
+            <table class ="tbl-full " >
+                <tr>
+                  <th>S/N</th>
+                  <th>Full Name</th>
+                  <th>UserName</th>
+                  <th>Actions</th>
+                  </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Jay john</td>  
+                  <td>hope</td>  
+                  <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2.</td>
+                  <td>Jay john</td>
+                  <td>hope</td>
+                  <td>
+                    update Admin
+                    Delete Admin
+                  </td>
+                </tr>
+                <tr>
+                  <td>3.</td>
+                  <td>Jay john</td>
+                  <td>hope</td>
+                  <td>
+                    update Admin
+                    Delete Admin
+                  </td>
+                </tr>
+              </table>   
+  </div>
+</div>
+<!-- Main content ends -->
+
+<?php include('partials/footer.php'); ?>
+
+
+
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!-- footer content ends -->
+<div class="footer">
+<div class="wrapper">
+            <p class="text-center">2020 all right reserved to foodhouse, Develope by <a href="https://.oparablessing.com.ng">Opara Blessing</a></p>
+         </div>
+    </div>
+<!-- footer content ends -->
+</body>
+</html>
